989d2cf4d050a0ced6bc2a1ffc1733b3b979fd7f
3keyroman <roman.cinkais@3key.company>
2022-09-15 12:10:56 +0200

    Add plantuml support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,19 @@ const config = {
   organizationName: '3KeyCompany', // Usually your GitHub org/user name.
   projectName: 'CZERTAINLY-Documentation', // Usually your repo name.
 
+  // plugins: [
+  //     './src/plugins/test'
+  // ],
   presets: [
     [
       '@docusaurus/preset-classic',
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [[require('mdx-mermaid'), {
-            theme: { light: 'neutral', dark: 'dark' }
-          }]],
+          remarkPlugins: [
+              [require('mdx-mermaid'), { theme: { light: 'neutral', dark: 'dark' } }],
+              [require('./src/plugins/remarkSimplePlantumlPlugin'), { baseUrl: 'https://www.plantuml.com/plantuml/svg', type: "svg" }]
+          ],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
