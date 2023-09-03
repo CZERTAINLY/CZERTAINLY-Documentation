3746ff92608417464c15c48c4c7494bc53b3bcf6
3keyroman <46850604+3keyroman@users.noreply.github.com>
2023-09-03 19:33:15 +0200

    Update release version to 2.9.0 (#107)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const apiVersion = 'develop';
+const apiVersion = '2.9.0';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
